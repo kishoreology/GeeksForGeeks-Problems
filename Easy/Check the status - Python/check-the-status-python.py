bceb3a3f6cfeb671cf57b8b4ffb555cdffadcf8d
kishore <106666554+kishoreology@users.noreply.github.com>
2024-05-05 10:06:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check the status - Python/check-the-status-python.py
@@ -0,0 +1,49 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+# Function to check value and 
+# return accordingly
+def check_status(a, b, flag):
+    ##Your code here
+    if (a>0) ^ (b>0) and flag==False:
+        return True
+    elif (a<0) and (b<0) and flag==True:
+        return True
+    else:
+        return False
+    ##Either True or False is returned
+
+#{ 
+ # Driver Code Starts.
+
+# Driver Code
+def main():
+    
+    # Testcase input
+    testcases = int(input())
+    
+    # Looping through testcases
+    while(testcases > 0):
+        a = int(input())
+        b = int(input())
+        flag = input()
+        
+        if(flag == "True"):
+            flag = True
+        else:
+            flag = False
+        
+        if(check_status(a, b, flag) is True):
+            print ("True")
+        else:
+            print ("False")
+        
+        testcases -= 1
+ 
+if __name__ == '__main__':
+    main()
+# } Driver Code Ends
