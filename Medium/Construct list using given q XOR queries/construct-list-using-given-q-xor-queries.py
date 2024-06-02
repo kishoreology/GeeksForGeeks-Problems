6767f906a08950edfd6f6d63f6fb8127f910414b
kishore <106666554+kishoreology@users.noreply.github.com>
2024-06-02 08:43:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct list using given q XOR queries/construct-list-using-given-q-xor-queries.py
@@ -0,0 +1,67 @@
+
+from typing import List
+
+class Solution:
+    def constructList(self, q : int, queries : List[List[int]]) -> List[int]:
+        xr,lis=0,[]
+        while queries:
+            a,b=queries.pop();
+            if a==0:
+                lis.append(b^xr)
+            else:
+                xr^=b
+        lis.append(xr)        
+        return sorted(lis)    
+
+
+
+#{ 
+ # Driver Code Starts
+class IntMatrix:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n, m):
+        matrix = []
+        #matrix input
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+
+    def Print(self, arr):
+        for i in arr:
+            for j in i:
+                print(j, end=" ")
+            print()
+
+
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        q = int(input())
+
+        queries = IntMatrix().Input(q, 2)
+
+        obj = Solution()
+        res = obj.constructList(q, queries)
+
+        IntArray().Print(res)
+
+# } Driver Code Ends
